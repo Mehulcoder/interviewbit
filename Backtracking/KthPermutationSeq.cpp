a34b5e88a5c017629ebe6e3b7b17dbc7b4872136
Mehulcoder <mehul355180@gmail.com>
2020-05-08 02:41:19 +0530

    Added Stuff

--- Backtracking/KthPermutationSeq.cpp
+++ Backtracking/KthPermutationSeq.cpp
@@ -1,25 +1,47 @@
-string helper(set<int> &s1, int k, string &out){
-	
+int fact(int n)
+{
+    if(n>12)
+        return INT_MAX;
+    if(n==0)
+        return 1;
+    return n*fact(n-1);
+}
 
+void helper(vector<int> &s1, int &k, string &out){
+	if (k==1)
+	{
+    	for (int i = 0; i < s1.size(); ++i)
+    	{
+    	    out+=to_string(s1[i]);
+    	}
+    	
+    	return;
+	}
 
+	int index = (k-1)/fact(s1.size()-1);
+	auto it = s1.begin();
+	int val = *(it+index);
 
+	k-=index*(fact(s1.size()-1));
+
+	out += to_string(val);
+	s1.erase(it+index);
+	helper(s1, k, out);
+	return;
 }
 
 string Solution::getPermutation(int A, int B) {
-	if (B>12)
-	{
-		return to_string(INT_MAX);
-	}
 
-	set<int> s1;
+	vector<int> s1;
 	
 	for (int i = 0; i < A; ++i)
     {
-	    s1.insert(i+1);
+	    s1.push_back(i+1);
     }
 
     string ans;
 
     helper(s1, B,ans);
+    
     return ans;
 }
