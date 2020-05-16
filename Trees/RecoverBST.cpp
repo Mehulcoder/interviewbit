472c64d0a2007377265d745cfd14ff1ad3d817fe
Mehulcoder <mehul355180@gmail.com>
2020-05-16 15:53:05 +0530

    Implemented morris Traversal

--- Trees/RecoverBST.cpp
+++ Trees/RecoverBST.cpp
@@ -7,26 +7,70 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-void helper(TreeNode* A, vector<int> &ans, int &prev, int &first, int &possibleSecond, int &vectorFilled){
-	if (A==NULL || vectorFilled)
+TreeNode* findPred(TreeNode* A){
+	if (A->left==NULL)
 	{
-		return;
+		return NULL;
 	}
 
-	helper(A->left, ans, prev, first, possibleSecond, vectorFilled);
-	if (prev>A->val && first)
-	{
-		ans.push_back(prev);
-		first = 0;
-		possibleSecond = A->val;
-	}else if (prev>A->val && !first)
-	{
-		ans.push_back(A->val);
-		vectorFilled = 1;
-		return;
+	TreeNode* temp = A->left;
+	while(temp->right && temp->right!=A){
+		temp = temp->right;
 	}
-	prev = A->val;
-	helper(A->right, ans, prev, first, possibleSecond, vectorFilled);
+
+	return temp;
+}
+
+void helper(TreeNode* A, vector<int> &ans, int &prev, int &first, int &possibleSecond, int &vectorFilled){
+
+	TreeNode* current = A;
+	while(current!=NULL){
+		if (vectorFilled)
+		{
+			return;
+		}
+		if (!current->left)
+		{
+			if (prev>current->val && first)
+			{
+				ans.push_back(prev);
+				first = 0;
+				possibleSecond = current->val;
+			}else if (prev>current->val && !first)
+			{
+				ans.push_back(current->val);
+				vectorFilled = 1;
+				return;
+			}
+			prev = current->val;
+			current = current->right;
+		}else{
+			TreeNode* predecessor = findPred(current);
+			if (!(predecessor->right))
+			{
+				predecessor->right = current;
+				current = current->left;
+			}else{
+				//Already visited
+				predecessor->right = NULL;
+				if (prev>current->val && first)
+				{
+					ans.push_back(prev);
+					first = 0;
+					possibleSecond = current->val;
+				}else if (prev>current->val && !first)
+				{
+					ans.push_back(current->val);
+					vectorFilled = 1;
+					return;
+				}
+				prev = current->val;
+				current = current->right;
+			}
+		}
+	}
+
+
 }
 
 vector<int> Solution::recoverTree(TreeNode* A) {
@@ -40,7 +84,9 @@ vector<int> Solution::recoverTree(TreeNode* A) {
 	{
 		ans.push_back(possibleSecond);
 	}
-
 	sort(ans.begin(), ans.end());
 	return ans;
 }
+
+
+// 15 10 7 13 6 8 12 15 -1 5 -1 -1 -1 -1 -1 -1
