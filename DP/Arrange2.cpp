f51002adb15cfbc09e97b56b057c0d6cc4b1bc10
Mehulcoder <mehul355180@gmail.com>
2020-05-24 12:30:37 +0530

    Added Stuff

---
+++ DP/Arrange2.cpp
@@ -0,0 +1,142 @@
+/*
+
+                Name: Mehul Chaturvedi
+                IIT-Guwahati
+
+*/
+
+/*
+                PROBLEM STATEMENT
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <chrono> 
+#ifndef mehul
+#pragma GCC optimize("Ofast")
+#endif
+
+typedef long long ll;
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<int, bool> umapib;
+typedef unordered_map<string, int> umapsi;
+typedef unordered_map<string, string> umapss;
+typedef map<string, int> mapsi;
+typedef map<pair<int, int>, int> mappiii;
+typedef map<int, int> mapii;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef unordered_set<int> useti;
+
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define uset unordered_set
+#define it iterator
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f first
+#define s second
+
+#define INF 4557430888798830399ll
+#define MOD 1000000007
+#define EPS 1e-7
+#define PI acos(-1)
+
+
+#define sz(x) (int)(x).size()
+ 
+#define fr(i, a, b) for (int i = (a), _b = (b); i <= _b; i++)
+#define rep(i, n) for (int i = 0, _n = (n); i < _n; i++)
+#define repr(i, n) for (int i = n - 1; i >= 0; i--)
+#define frr(i, a, b) for (int i = (a), _b = (b); i >= _b; i--)
+#define trav(a, x) for(auto& a : x)
+#define fil(ar, val) memset(ar, val, sizeof(ar))  // 0x3f for inf, 0x80 for -INF can also use with pairs
+
+#ifdef mehul
+template<typename T>
+void __p(T a) { cout << a << " "; }
+template<typename T>
+void __p(std::vector<T> a) { cout << "{ "; for (auto p : a) __p(p); cout << "}"; }
+template<typename T, typename F>
+void __p(pair<T, F> a) { cout << "{ "; __p(a.first); __p(a.second); cout << "}"; }
+template<typename T, typename ...Arg>
+void __p(T a1, Arg ...a) { __p(a1); __p(a...); }
+template<typename Arg1>
+void __f(const char *name, Arg1 &&arg1) {
+	cout<<name<<" : ";__p(arg1);cout<<endl;
+}
+template<typename Arg1, typename ... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+	int bracket=0,i=0;
+	for(; ;i++)
+		if(names[i]==','&&bracket==0)
+			break;
+		else if(names[i]=='(')
+			bracket++;
+		else if(names[i]==')')
+			bracket--;
+	const char *comma=names+i;
+	cout.write(names,comma-names)<<" : ";
+	__p(arg1);
+	cout<<"| ";
+	__f(comma+1,args...);
+}
+#define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
+int begtime = clock();
+#define end_routine() cout << "\n\nTime elapsed: " << (clock() - begtime)*1000/CLOCKS_PER_SEC << " ms\n\n";
+#else
+#define endl '\n'
+#define trace(...)
+#define end_routine()
+#endif
+
+int getCount(string s1){
+	int w = 0;
+	int b = 0;
+	rep(i, s1.size()){
+		w+=s1[i]=='W';
+		b+=s1[i]=='B';
+	}
+
+	return w*b;
+}
+
+int Solution::arrange(string A, int B) {
+	int n = A.size();
+	vector<vector<int>>dp(B+1, vector<int>(n, -1));
+	//Fill base case j==1;
+	repr(i, n-1){
+		dp[1][i] = getCount(A.substr(i));
+	}
+
+	//Fill for j==B starting from i == 2;
+	dp[1][n-1] = 0;
+	fr(i, 2, B){
+		dp[i][n-1] = -1;
+	}
+
+	//fill from i==2 and j==n-2;
+	fr(i, 2, B){
+		repr(j, n-1){
+			dp[i][j] = INT_MAX;
+			fr(l, j+1, n-1){
+				if (dp[i-1][l]==-1)
+				{
+					continue;
+				}
+				dp[i][j] = min(dp[i][j], dp[i-1][l]+getCount(A.substr(j, l-j)));
+				
+			}
+			if (dp[i][j]==INT_MAX)
+			{
+				dp[i][j] = -1;
+			}
+		}
+	}
+
+
+
+	return dp[B][0];
+}
+
