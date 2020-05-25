0190089eb1db52427d47b9c1f4fd7587cebc3751
Mehulcoder <mehul355180@gmail.com>
2020-05-25 14:11:52 +0530

    Added Stuff

---
+++ DP/MaxSumnWithoutAdjacent.cpp
@@ -0,0 +1,137 @@
+/*
+
+                Name: Mehul Chaturvedi
+                IIT-Guwahati
+
+*/
+
+/*
+                PROBLEM STATEMENT
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <chrono> 
+#ifndef mehul
+#pragma GCC optimize("Ofast")
+#endif
+
+typedef long long ll;
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<int, bool> umapib;
+typedef unordered_map<string, int> umapsi;
+typedef unordered_map<string, string> umapss;
+typedef map<string, int> mapsi;
+typedef map<pair<int, int>, int> mappiii;
+typedef map<int, int> mapii;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef unordered_set<int> useti;
+
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define uset unordered_set
+#define it iterator
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f first
+#define s second
+
+#define INF 4557430888798830399ll
+#define MOD 1000000007
+#define EPS 1e-7
+#define PI acos(-1)
+
+
+#define sz(x) (int)(x).size()
+ 
+#define fr(i, a, b) for (int i = (a), _b = (b); i <= _b; i++)
+#define rep(i, n) for (int i = 0, _n = (n); i < _n; i++)
+#define repr(i, n) for (int i = n; i >= 0; i--)
+#define frr(i, a, b) for (int i = (a), _b = (b); i >= _b; i--)
+#define trav(a, x) for(auto& a : x)
+#define fil(ar, val) memset(ar, val, sizeof(ar))  // 0x3f for inf, 0x80 for -INF can also use with pairs
+
+#ifdef mehul
+template<typename T>
+void __p(T a) { cout << a << " "; }
+template<typename T>
+void __p(std::vector<T> a) { cout << "{ "; for (auto p : a) __p(p); cout << "}"; }
+template<typename T, typename F>
+void __p(pair<T, F> a) { cout << "{ "; __p(a.first); __p(a.second); cout << "}"; }
+template<typename T, typename ...Arg>
+void __p(T a1, Arg ...a) { __p(a1); __p(a...); }
+template<typename Arg1>
+void __f(const char *name, Arg1 &&arg1) {
+	cout<<name<<" : ";__p(arg1);cout<<endl;
+}
+template<typename Arg1, typename ... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+	int bracket=0,i=0;
+	for(; ;i++)
+		if(names[i]==','&&bracket==0)
+			break;
+		else if(names[i]=='(')
+			bracket++;
+		else if(names[i]==')')
+			bracket--;
+	const char *comma=names+i;
+	cout.write(names,comma-names)<<" : ";
+	__p(arg1);
+	cout<<"| ";
+	__f(comma+1,args...);
+}
+#define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
+int begtime = clock();
+#define end_routine() cout << "\n\nTime elapsed: " << (clock() - begtime)*1000/CLOCKS_PER_SEC << " ms\n\n";
+#else
+#define endl '\n'
+#define trace(...)
+#define end_routine()
+#endif
+
+//Alternate approach
+/*
+We traverse column by column and maintain maximum sum considering two cases.
+1) An element of current column is included. In this case we take maximum of two elements in current column.
+2) An element of current column is excluded (or not included)
+*/
+
+int Solution::adjacent(vector<vector<int> > &A) {
+	int n = A[0].size();
+	int ans = INT_MIN;
+	vector<vector<int>> incl(2, (vector<int>(A[0].size(), 0)));
+	vector<vector<int>> excl(2, (vector<int>(A[0].size(), 0)));
+	//Base case
+	if (n==1)
+	{
+		return max(A[0][0], A[1][0]);
+	}
+	//Set 4 starting values for both of them
+	incl[0][0] = A[0][0];
+	incl[1][0] = A[1][0];
+	incl[0][1] = A[0][1];
+	incl[1][1] = A[1][1];
+
+	excl[0][0] = 0;
+	excl[1][0] = A[0][0];
+	excl[0][1] = max(A[1][0], A[0][0]);
+	excl[1][1] = max(incl[0][1], excl[0][1]);
+	
+	fr(j, 2, n-1){
+		rep(i, 2){
+			if (i==0)
+			{
+			    
+				incl[i][j] = A[i][j] + excl[i][j-1];
+				excl[i][j] = max(incl[1][j-1], excl[1][j-1]);
+			}else{
+				incl[i][j] = A[i][j] + excl[0][j-1];
+				excl[i][j] = max(incl[0][j], excl[0][j]);	
+			}
+		}
+	}
+
+	return ans;
+}
