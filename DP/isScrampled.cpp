87cb11cdafdf4e22fa8dab333fa31284411f32bd
Mehulcoder <mehul355180@gmail.com>
2020-06-01 17:55:32 +0530

    Added stuff

---
+++ DP/isScrampled.cpp
@@ -0,0 +1,133 @@
+/*
+
+                Name: Mehul Chaturvedi
+                IIT-Guwahati
+
+*/
+
+/*
+                PROBLEM STATEMENT
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <chrono> 
+#ifndef mehul
+#pragma GCC optimize("Ofast")
+#endif
+
+typedef long long ll;
+typedef unordered_map<int, int> umapii;
+typedef unordered_map<int, bool> umapib;
+typedef unordered_map<string, int> umapsi;
+typedef unordered_map<string, string> umapss;
+typedef map<string, int> mapsi;
+typedef map<pair<int, int>, int> mappiii;
+typedef map<int, int> mapii;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef unordered_set<int> useti;
+
+#define debug(x) cout << '>' << #x << ':' << x << endl;
+#define uset unordered_set
+#define it iterator
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f first
+#define s second
+
+#define INF 4557430888798830399ll
+#define MOD 1000000007
+#define EPS 1e-7
+#define PI acos(-1)
+
+
+#define sz(x) (int)(x).size()
+ 
+#define fr(i, a, b) for (int i = (a), _b = (b); i <= _b; i++)
+#define rep(i, n) for (int i = 0, _n = (n); i < _n; i++)
+#define repr(i, n) for (int i = n; i >= 0; i--)
+#define frr(i, a, b) for (int i = (a), _b = (b); i >= _b; i--)
+#define trav(a, x) for(auto& a : x)
+#define fil(ar, val) memset(ar, val, sizeof(ar))  // 0x3f for inf, 0x80 for -INF can also use with pairs
+
+#ifdef mehul
+template<typename T>
+void __p(T a) { cout << a << " "; }
+template<typename T>
+void __p(std::vector<T> a) { cout << "{ "; for (auto p : a) __p(p); cout << "}"; }
+template<typename T, typename F>
+void __p(pair<T, F> a) { cout << "{ "; __p(a.first); __p(a.second); cout << "}"; }
+template<typename T, typename ...Arg>
+void __p(T a1, Arg ...a) { __p(a1); __p(a...); }
+template<typename Arg1>
+void __f(const char *name, Arg1 &&arg1) {
+	cout<<name<<" : ";__p(arg1);cout<<endl;
+}
+template<typename Arg1, typename ... Args>
+void __f(const char *names, Arg1 &&arg1, Args &&... args) {
+	int bracket=0,i=0;
+	for(; ;i++)
+		if(names[i]==','&&bracket==0)
+			break;
+		else if(names[i]=='(')
+			bracket++;
+		else if(names[i]==')')
+			bracket--;
+	const char *comma=names+i;
+	cout.write(names,comma-names)<<" : ";
+	__p(arg1);
+	cout<<"| ";
+	__f(comma+1,args...);
+}
+#define trace(...) cout<<"Line:"<<__LINE__<<" "; __f(#__VA_ARGS__, __VA_ARGS__)
+int begtime = clock();
+#define end_routine() cout << "\n\nTime elapsed: " << (clock() - begtime)*1000/CLOCKS_PER_SEC << " ms\n\n";
+#else
+#define endl '\n'
+#define trace(...)
+#define end_routine()
+#endif
+int helper(string A, string B, unordered_map<string, int>& m1){
+	if (A==B)
+	{
+		return 1;
+	}
+	string cache = A+"|"+B;
+	if (m1.find(cache)!=m1.end())
+	{
+		// trace(cache, m1[cache]);
+		return m1[cache];
+	}
+
+	int m = A.size();
+	int n = B.size();
+	if (m!=n)
+	{
+		return 0;
+	}
+
+	rep(i, n-1){
+		int val1 = helper(A.substr(0, i+1), B.substr(0, i+1), m1) && helper(A.substr(i+1), B.substr(i+1), m1);
+		// trace(val1, A.substr(0, i+1), B.substr(0, i+1),A.substr(i+1), B.substr(i+1));
+		
+		int val2 = helper(A.substr(0, n-i-1), B.substr(i+1), m1) && helper(A.substr(n-i-1), B.substr(0, i+1), m1);
+		// trace(val2, A.substr(0, n-i-1), B.substr(i+1),A.substr(n-i-1), B.substr(0, i+1));
+
+		if (val1||val2)
+		{
+			m1[cache] = 1;
+			return 1;
+		}
+	}
+	m1[cache] = 0;
+	return 0;
+}
+
+int Solution::isScramble(const string A, const string B) {
+    unordered_map<string, int> m1;	
+	return helper(A, B, m1);
+
+}
